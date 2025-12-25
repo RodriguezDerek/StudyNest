f75d243d2578bb5fbb75c495a3f7ec6406b467b8
Derek Rodriguez <derekirodriguez05@gmail.com>
2025-12-25 16:16:37 -0500

    Frontend folder structured

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+export default function Navbar() {
+
+    return (
+        <div>
+            <h1>Navbar</h1>
+            <p>Welcome to the navbar component!</p>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Calendar.tsx
@@ -0,0 +1,10 @@
+
+export default function Calendar() {
+
+    return (
+        <div>
+            <h1>Calendar</h1>
+            <p>Welcome to the calendar page!</p>
+        </div>
+    );
+}
